8935e22182543375041c335e5c3a07625ef9e02a
Ariful Islam <remalarif0130@gmail.com>
2025-09-30 20:36:48 +0600

    start

---
+++ V-01/app.js
@@ -0,0 +1,7 @@
+function add(num1, num2, printResult) {
+    return num1 + num2;
+}
+var nn = 20;
+var nnn = 10;
+var printResult = true;
+console.log(add(nn, nnn, printResult));
---
+++ V-01/app.ts
@@ -0,0 +1,11 @@
+function add(num1:number, num2:number,printResult:boolean) {
+if(printResult){
+    console.log(num1 + num2)
+}
+    return num1 + num2
+}
+
+const nn = 20;
+const nnn = 10;
+const printResult = true
+console.log(add(nn, nnn,printResult));
